9406ac72b6fef990e97de17db6b7fdfe6e2631e6
zxc58 <abc01234567889@gmail.com>
2021-12-29 19:30:20 +0800

    122903

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express") ;
 const app = express() ;
 const port = 3000 ;
+const exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 app.get("/",(req,res)=>{
     res.end("ResEnd");
