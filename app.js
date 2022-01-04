3375cc83a859c1100c2cc8f01282d980f029c615
zxc58 <abc01234567889@gmail.com>
2022-01-04 17:49:42 +0800

    2022010401

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 const express = require("express") ;
 const app = express() ;
 const port = 3000 ;
+const restaurants = require("./restaurant.json").results; 
+//console.log(restaurants);
 const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-app.get("/",(req,res)=>{
-    res.end("ResEnd");
+app.use(express.static('public'));
+
+app.get("/restaurants",(req,res)=>{
+    res.render("index",{restaurants:restaurants});
 });
+app.get("/restaurants/:id",(req,res)=>{
+    let id = req.params.id;
+    let findResult = restaurants.find(index=>index.id.toString()===id);
+    res.render("show",{restaurant:findResult});
+});
+
+app.get("/search",(req,res)=>{
+    let search_value = req.query.keyword.toLowerCase();
+    console.log(search_value);
+    let restaurants2 = restaurants.filter(index=>{
+        return index.name.toLowerCase().includes(search_value)
+        ||index.category.toLowerCase().includes(search_value);
+    });
+    console.log(restaurants2);
+    res.render("index",{restaurants:restaurants2});
+});    
+
+
+
 app.listen(port,()=>{
-    console.log("http://localhost:"+port);
+    console.log("http://localhost:"+port+"/restaurants");
 });
