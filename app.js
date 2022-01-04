c93cd1d3287dbb11abcd25da80893b1efe72478e
zxc58 <abc01234567889@gmail.com>
2022-01-04 19:16:00 +0800

    2022010402

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express") ;
 const app = express() ;
 const port = 3000 ;
 const restaurants = require("./restaurant.json").results; 
-//console.log(restaurants);
 const exphbs = require('express-handlebars');
+
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
-
 app.use(express.static('public'));
 
 app.get("/restaurants",(req,res)=>{
@@ -20,13 +20,11 @@ app.get("/restaurants/:id",(req,res)=>{
 
 app.get("/search",(req,res)=>{
     let search_value = req.query.keyword.toLowerCase();
-    console.log(search_value);
-    let restaurants2 = restaurants.filter(index=>{
+    let search_result = restaurants.filter(index=>{
         return index.name.toLowerCase().includes(search_value)
         ||index.category.toLowerCase().includes(search_value);
     });
-    console.log(restaurants2);
-    res.render("index",{restaurants:restaurants2});
+    res.render("index",{restaurants:search_result,search_value:req.query.keyword});
 });    
 
 
