1470db4d123c3bedec2591d175932ed490a3fcc1
zxc58 <abc01234567889@gmail.com>
2022-01-06 16:55:50 +0800

    更改title 加一些註解

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
+//設定模組 常數
 const express = require("express") ;
 const app = express() ;
 const port = 3000 ;
 const restaurants = require("./restaurant.json").results; 
 const exphbs = require('express-handlebars');
 
-
+//express設定
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
+//路由設定
 app.get("/restaurants",(req,res)=>{
     res.render("index",{restaurants});
 });
@@ -24,6 +26,7 @@ app.get("/search",(req,res)=>{
     res.render("index",{restaurants:search_result,search_value:req.query.keyword});
 });    
 
+//啟動監聽
 app.listen(port,()=>{
     console.log("server start at http://localhost:"+port+"/restaurants");
 });
