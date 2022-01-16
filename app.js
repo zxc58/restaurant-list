cbac4237af8cb4e97b9c04fb6add191ba9875eb8
zxc58 <abc01234567889@gmail.com>
2022-01-16 22:17:20 +0800

    20220116 2200 完善從db抓資料功能

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //require模組 設定常數
 const express = require("express") ;
 const exphbs = require("express-handlebars");
+const { render } = require("express/lib/response");
 const mongoose = require("mongoose");
 const Restaurant = require("./models/restaurant");
 //const restaurants = require("./restaurant.json").results; 
@@ -14,7 +15,6 @@ db.on("error",()=>{
     console.log("db error");
 });
 db.once("open",()=>{
-    console.log("db open");
 });
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
@@ -24,27 +24,31 @@ app.use(express.static('public'));
 //路由設定
 app.get("/restaurants",(req,res)=>{
     Restaurant.find().lean().then(restaurants=>{
-        //console.log(restaurants);
         res.render("index",{restaurants});
+        //console.log(restaurants);
     })
     .catch(error=>{
-        console.log(error);
+        console.log("01 error");
     });
-    
 });
-app.get("/restaurants/:id",(req,res)=>{
-    const restaurant = restaurants.find(each=>each.id.toString()===req.params.id);
-    res.render("show",{restaurant});
+app.get("/restaurants/:_id",(req,res)=>{
+    Restaurant.findById(req.params._id).lean().then(restaurant=>{
+        res.render("show",{restaurant});
+    })
+    .catch(err=>console.log("04err"));
 });
 app.get("/search",(req,res)=>{
-    const search_value = req.query.keyword.toLowerCase();
-    const search_result = restaurants.filter(each=>
-        each.name.toLowerCase().includes(search_value)||each.category.toLowerCase().includes(search_value)||each.name_en.toLowerCase().includes(search_value)
-    );
-    res.render("index",{restaurants:search_result,search_value:req.query.keyword});
+    const search_value = new RegExp(req.query.keyword.toLowerCase(),"i");
+    Restaurant.find(
+        {$or: [{name:search_value},{name_en:search_value},{category:search_value}]}
+    )
+    .lean().then(search_result=>{
+        res.render("index",{restaurants:search_result,search_value:req.query.keyword});
+    })
+    .catch(err=>console.log("05err"));
 });    
 
 //啟動監聽
 app.listen(port,()=>{
-    console.log("server start at http://localhost:"+port+"/restaurants");
+    console.log(`server start at http://localhost:${port}/restaurants`);
 });
