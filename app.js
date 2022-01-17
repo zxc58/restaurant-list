662d9d457de67591ae7cae171e45dde43a9f23ee
zxc58 <abc01234567889@gmail.com>
2022-01-17 21:53:19 +0800

    202201172200 工作內容 route handlebars 完成

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 //require模組 設定常數
 const express = require("express") ;
 const exphbs = require("express-handlebars");
-const { render } = require("express/lib/response");
+const { redirect } = require("express/lib/response");
 const mongoose = require("mongoose");
 const Restaurant = require("./models/restaurant");
-//const restaurants = require("./restaurant.json").results; 
 const app = express() ;
 const port = 3000 ;
 const db = mongoose.connection;
@@ -16,7 +15,7 @@ db.on("error",()=>{
 });
 db.once("open",()=>{
 });
-
+app.use(express.urlencoded({extended:true}));
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.use(express.static('public'));
@@ -31,12 +30,7 @@ app.get("/restaurants",(req,res)=>{
         console.log("01 error");
     });
 });
-app.get("/restaurants/:_id",(req,res)=>{
-    Restaurant.findById(req.params._id).lean().then(restaurant=>{
-        res.render("show",{restaurant});
-    })
-    .catch(err=>console.log("04err"));
-});
+
 app.get("/search",(req,res)=>{
     const search_value = new RegExp(req.query.keyword.toLowerCase(),"i");
     Restaurant.find(
@@ -47,7 +41,35 @@ app.get("/search",(req,res)=>{
     })
     .catch(err=>console.log("05err"));
 });    
-
+app.get("/restaurants/new",(req,res)=>{
+    res.render("newedit",{action:`/restaurants/new`,page:"New Page"});
+});
+app.get("/restaurants/:_id",(req,res)=>{
+    Restaurant.findById(req.params._id).lean().then(restaurant=>{
+        res.render("show",{restaurant});
+    })
+    .catch(err=>console.log("04err"));
+});
+app.get("/restaurants//edit/:_id",(req,res)=>{
+    //console.log("123");
+    Restaurant.findById(req.params._id).lean().then(restaurant=>{
+        res.render("newedit",{restaurant,action:`/restaurants/${req.params._id}/edit`,page:"Edit Page"});
+    })
+    .catch(err=>console.log("07err"));
+});
+app.post("/restaurants/:_id/edit",(req,res)=>{
+    //check req.body 內容物
+    Restaurant.findById(req.params._id).then(restaurant=>{
+        restaurant=req.body;
+        return restaurant.save();
+    })
+    .then(()=>{ redirect("/restaurants/"+req.params._id); })
+    .catch(err=>("11err"));
+});
+app.post("/restaurants/new",(req,res)=>{
+    //check req.body 內容物
+    Restaurant.create(req.body).then(()=>redirect("/")).catch(err=>console.log("08err"));
+});
 //啟動監聽
 app.listen(port,()=>{
     console.log(`server start at http://localhost:${port}/restaurants`);
