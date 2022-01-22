1b8f545611f88923df6ee21cc5b7a68c488334b4
zxc58 <abc01234567889@gmail.com>
2022-01-22 14:05:28 +0800

    202201221400

--- myMiddleware.js
+++ myMiddleware.js
@@ -1,22 +1,39 @@
 module.exports = {
     validationGuard:(req,res,next)=>{
+        console.log(req.url);
         if(req.method==="GET"||req.method==="DELETE")
             next();
         else if(
             (/^[\w\s]+$/.test(req.body.name_en))&&
             (/^[\d\s]+$/.test(req.body.phone))&&
-            (0<=req.body.rating<=5)&&
+            (0<=parseFloat(req.body.rating)<=5)&&
             (req.body.name!="")&&
             (req.body.category!="")&&
             (req.body.location!="")&&
             (req.body.image!="")&&
             (req.body.google_map!="")&&
             (req.body.description!="")
-        ){
-            console.log("bp45/4t/6ej/ ")
+        )
             next();
+        else{
+            /*if(req.body.this_req_sendby==="Edit Page"){
+                const method = "?_method=PUT"
+                res.render("newedit",{
+                    restaurant:req.body,
+                    action:`/restaurants/${req.params._id}${method}`,
+                    page:"Edit Page",
+                    message:"後端認證不通過"
+                });
+            }
+            else{
+                res.render("newedit",{
+                    restaurant:req.body,
+                    action:`/restaurants`,
+                    page:"New Page",
+                    message:"後端認證不通過"
+                });
+            }*/
+            
         }
-        else
-        console.log("認證沒過");
     }
 }
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -10,7 +10,7 @@ router.get("/",(req,res)=>{
         search_value = new RegExp(req.query.keyword,"i");
         a={$or: [{name:search_value},{name_en:search_value},{category:search_value}]};
     }
-    Restaurant.find(a).lean().then(restaurants=>{
+    Restaurant.find(a).lean().sort({name:"asc"}).then(restaurants=>{
         res.render("index",{restaurants,search_value:req.query.keyword});
     })
     .catch(error=>{
