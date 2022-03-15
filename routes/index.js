6f26cc60ad223224065b7957450320020dce4b24
zxc58 <abc01234567889@gmail.com>
2022-03-15 17:24:39 +0800

    先掛載登入註冊路由

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const home = require("./modules/home");
 const resetaurants = require("./modules/restaurants");
+const user = require("./modules/user")
 
-router.use("/",home);
+router.use("/user",user)
 router.use("/restaurants",resetaurants);
-
+router.use("/",home);
 module.exports = router ;
 
 
---
+++ routes/modules/user.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+ 
+router.get("/signin",(req,res)=>{
+    
+})
+router.get("/signup",(req,res)=>{
+
+})
+router.post("/signin",(req,res)=>{
+
+})
+router.post("/signup",(req,res)=>{
+
+})
+router.get("/signout",(req,res)=>{
+
+})
+module.exports=router
