b16e7891f0606adbb9414ec77227b1c6f347a085
zxc58 <abc01234567889@gmail.com>
2022-03-24 13:39:05 +0800

    登入後只顯示"自己的"資料功能

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,9 +4,9 @@ const Restaurant = require('../../models/restaurant')
 
 router.get('/', (req, res) => {
   let a, search_value
-  if (!req.query.keyword) { a = {} } else {
+  if (!req.query.keyword) { a = {userId:req.user._id} } else {
     search_value = new RegExp(req.query.keyword, 'i')
-    a = { $or: [{ name: search_value }, { name_en: search_value }, { category: search_value }] }
+    a = { userId:req.user._id,$or: [{ name: search_value }, { name_en: search_value }, { category: search_value }] }
   }
   Restaurant.find(a).lean().sort({ name: 'asc' }).then(restaurants => {
     const db_result = JSON.stringify(restaurants)
