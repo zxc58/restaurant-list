8d5b96366a6f89073a88274097bc1be4bbcc3954
zxc58 <abc01234567889@gmail.com>
2022-03-31 20:43:00 +0800

    修改seeder內容

--- models/seeds/restaurant_seeder.js
+++ models/seeds/restaurant_seeder.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const db = require('../../config/mongoose')
 const restaurants = require('../../restaurant.json').results
 const Restaurant = require('../restaurant')
-const user = require('../../user.json').user
+const users = require('../../user.json').results
 const User = require('../user')
 const bcrypt = require('bcryptjs')
 //
@@ -11,13 +11,18 @@ db.on('error', () => {
   console.log('seeder error')
 })
 db.once('open', async () => {
-  for (let i = 0; i < 2; i++) {
-    await bcrypt.genSalt(5).then(salt => bcrypt.hash(user[i].password, salt)).then(hash => { user[i].password = hash })
+  for (let i = 0; i < users.length; i++) {
+    const salt = await bcrypt.genSalt(5)
+    const hashed = await bcrypt.hash(users[i].password, salt)
+    users[i].password = hashed
   }
-  const usersData = await User.create(user)
-  const [user1Id, user2Id] = [usersData[0]._id, usersData[1]._id]
-  for (let i = 0; i < 8; i++) {
-    if (i < 3 || i > 5) { restaurants[i].userId = user1Id } else { restaurants[i].userId = user2Id }
+  const usersData = await User.create(users)
+  const usersIds = usersData.map(e => e._id)
+  
+  for (let i = 0; i < users.length; i++) {
+    users[i].restaurantsIds.forEach(e => {
+      restaurants[e - 1].userId = usersIds[i]
+    })
   }
   await Restaurant.create(restaurants)
   console.log('seed success')
